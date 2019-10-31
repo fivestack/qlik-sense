894dd327f94fdbaebf3276d6c4cce85edb37a98d
Mike Alfare <mike.alfare@markel.com>
2019-10-31 14:32:20 -0400

    refactored Client and broke out Auth

--- qlik_sense/client.py
+++ qlik_sense/client.py
@@ -6,15 +6,42 @@ then passed up to this class to execute.
 import logging
 import sys
 import os.path
-import urllib.parse
 import random
 import string
+from typing import Optional, Union
+import json
 
 import urllib3
+from urllib3.util import Url
 import requests
 from requests_ntlm import HttpNtlmAuth
 
 from qlik_sense import services
+
+
+class _Auth:
+
+    def __init__(self, directory: str = None, username: str = None, password: str = None,
+                 certificate: str = None, verify: bool = False):
+        self._directory = directory
+        self._username = username
+        self._password = password
+        self._certificate = certificate
+        self.verify = verify
+
+    def qlik_user(self) -> str:
+        return f'UserDirectory={self._directory}; UserId={self._username}'
+
+    def ntlm_auth(self) -> 'Optional[HttpNtlmAuth]':
+        if self._directory and self._username and self._password:
+            return HttpNtlmAuth(username=f'{self._directory}\\{self._username}', password=self._password)
+        return
+
+    def cert(self) -> 'Optional[tuple]':
+        if self._certificate:
+            (base, ext) = os.path.splitext(self._certificate)
+            return f'{base}{ext}', f'{base}_key{ext}'
+        return
 
 
 class Client:
@@ -22,7 +49,7 @@ class Client:
     An interface over the QlikSense APIs
 
     Args:
-        schema: http/https
+        scheme: http/https
         host: hostname to connect to
         port: port number
         certificate: path to .pem client certificate
@@ -32,25 +59,29 @@ class Client:
         verbosity: level of logging, one of [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
     """
 
-    _referer = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) qsAPI APIREST (QSense)'
-    _session = None
-    _cert = None
-    _verify = False
-    _qlik_user = None
+    _log = None
 
     def __init__(self,
-                 schema: str, host: str, port: int,
-                 certificate: str = None, user: dict = None, verify: bool = False,
+                 scheme: str, host: str, port: int,
+                 user: dict = None, certificate: str = None, verify: bool = False,
                  log_name: str = None, verbosity: int = None):
         if log_name:
             self._set_logger(log_name=log_name, verbosity=verbosity)
-        self._baseurl = f'{schema}://{host}:{str(port)}'
-        self._session = requests.Session()
-        self._set_auth(certificate=certificate, user=user)
-        self._verify = verify
-        if not self._verify:
+        self._log.debug('__SET LOGGING DONE')
+
+        self._log.debug('__SET AUTH')
+        self._auth = _Auth(**user, certificate=certificate, verify=verify)
+
+        if not self._auth.verify:
+            self._log.debug('__DISABLED WARNINGS')
             urllib3.disable_warnings()
 
+        self._log.debug('__SET BASE URL')
+        self._scheme = scheme
+        self._host = host
+        self._port = port
+
+        self._log.debug('__SET SERVICES')
         self.app = services.AppService(self)
 
     def _set_logger(self, log_name: str, verbosity: int):
@@ -61,141 +92,44 @@ class Client:
             log_name: name of the log
             verbosity: level of logging, one of [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
         """
-        self._log = logging.getLogger(log_name)
-        if not self._log.hasHandlers():
-            self._log.addHandler(logging.StreamHandler(sys.stdout))
+        self._log = logging.getLogger(name=log_name)
         self._log.setLevel(verbosity)
-        self._log.info('SET LOGGER')
+        self._log.addHandler(logging.StreamHandler(sys.stdout))
 
-    def _set_auth(self, certificate: str, user: dict):
-        """
-        Sets up authentication for the controller. Routes based on whether a certificate or a user was provided
-
-        Args:
-            certificate: path to .pem client certificate
-            user: a dictionary with user credentials, in the form {'directory': '', 'username': '', 'password': ''}
-        """
-        self._log.debug(f'__SET AUTH')
-        if certificate and user:
-            self._log.info('INVALID authentication provided')
-            raise AttributeError('Provide only one of certificate (for PEM) and user (for NTLM)')
-        elif user:
-            self._log.info('NTLM authentication enabled')
-        elif certificate:
-            self._log.info('PEM authentication enabled')
-            self._set_cert(certificate=certificate)
-            user = {'directory': None, 'username': None, 'password': None}
-        else:
-            self._log.info('NO authentication enabled')
-            user = {'directory': None, 'username': None, 'password': None}
-        self._set_user(user=user)
-
-    def _set_cert(self, certificate: str):
-        """
-        Sets up PEM authentication for the controller
-
-        Args:
-            certificate: path to .pem client certificate
-        """
-        self._log.debug(f'__SET CERTIFICATE certificate={certificate}')
-        (base, ext) = os.path.splitext(certificate)
-        self._cert = (base + ext, base + '_key' + ext)
-        self._log.debug(f'CERTKEY: {base}.{ext}')
-
-    def _set_user(self, user: dict):
-        """
-        Sets up NTLM authentication for the controller
-
-        Args:
-            user: a dictionary with user credentials, in the form {'directory': '', 'username': '', 'password': ''}
-        """
-        self._log.debug(f'__SET USER')
-        directory = user.get('directory')
-        username = user.get('username')
-        password = user.get('password')
-        self._qlik_user = f'UserDirectory={directory}; UserId={username}'
-        self._session.auth = HttpNtlmAuth(username=f'{directory}\\{username}', password=password)
-
-    def _prepare_params(self, params: dict = None) -> dict:
-        """
-        Builds the url parameters for the request
-
-        Args:
-            params: the starting url parameters
-
-        Returns: the updated url parameters
-        """
-        self._log.debug(f'__PREPARE PARAMS params={params}')
-        updated_params = {'Xrfkey': ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16))}
-        if params:
-            for p, v in params.items():
-                if v is not None:
-                    if isinstance(v, bool):
-                        updated_params[p] = str(v).lower()
-                    else:
-                        updated_params[p] = str(v)
-                    self._log.debug(f' >> {p}=>{updated_params[p]}')
-                else:
-                    self._log.debug(f' >> {p}=>(default)')
-        return updated_params
-
-    def _prepare_headers(self, headers: dict = None) -> dict:
-        """
-        Builds the headers for the request
-
-        Args:
-            headers: the starting headers
-
-        Returns: the updated headers
-        """
-        self._log.debug(f'__PREPARE HEADERS headers={headers}')
-        updated_headers = {
-            'User-agent': self._referer,
-            'Pragma': 'no-cache',
-            'X-Qlik-User': self._qlik_user,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-        if headers:
-            updated_headers.update(headers)
-        return headers
-
-    def _prepare_request(self, url: str, params: dict) -> tuple:
+    def _prepare_request(self, method: str, url: str, params: dict, data: str) -> 'requests.Request':
         """
         Builds the url, url parameters, and headers for the request
 
         Args:
+            method: REST method, one of ['GET', 'POST', 'PUT', 'DELETE']
             url: the starting url
             params: the starting url parameters
-            headers: the starting headers
+            data: data to be inserted in the body of the request
 
         Returns: a tuple with the updated url, url parameters, and headers
         """
-        self._log.debug(f'__PREPARE REQUEST <{url}> params={params}')
-        updated_params = self._prepare_params(params=params)
-        headers = {'x-Qlik-Xrfkey': updated_params.get('Xrfkey')}
-        updated_headers = self._prepare_headers(headers=headers)
-        updated_url = self._update_params(url=urllib.parse.urljoin(self._baseurl, url), params=updated_params)
-        return updated_url, updated_params, updated_headers
+        xrf_key = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16))
 
-    def _update_params(self, url: str, params: dict) -> str:
-        """
-        Updates url parameters with new values.
+        url = Url(scheme=self._scheme, host=self._host, port=self._port, path=url)
+        headers = {
+            'User-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) qsAPI APIREST (QSense)',
+            'Pragma': 'no-cache',
+            'X-Qlik-User': self._auth.qlik_user,
+            'x-Qlik-Xrfkey': xrf_key,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
 
-        Args:
-            url: the request url
-            params: the parameters to update (and their new values)
+        request = requests.Request(method=method,
+                                   url=url,
+                                   headers=headers,
+                                   data=data,
+                                   params=params.update({'Xrfkey': xrf_key}),
+                                   auth=self._auth.ntlm_auth())
+        return request
 
-        Returns: the updated request url
-        """
-        self._log.debug(f'__UPDATE PARAMS <{url}> params={params}')
-        scheme, netloc, path, query, fragment = urllib.parse.urlsplit(url)
-        p = urllib.parse.parse_qs(query)
-        p.update(params)
-        query = urllib.parse.urlencode(p, doseq=True, quote_via=urllib.parse.quote)
-        return urllib.parse.urlunsplit((scheme, netloc, path, query, fragment))
-
-    def call(self, method: str, url: str, params: dict = None, data: str = None) -> 'requests.Response':
+    def call(self, method: str, url: str, params: 'Optional[dict]' = None,
+             data: 'Optional[Union[str, list, dict]]' = None) -> 'requests.Response':
         """
         All requests are routed through this method.
 
@@ -207,39 +141,22 @@ class Client:
 
         Returns: a Response object
         """
-        self._log.info(f'API {method} <{url}> params={params} data={len(data) if data else None}')
-        updated_url, updated_params, updated_headers = self._prepare_request(url=url, params=params)
+        self._log.info(f'API CALL {method} <{url}> params={params} data={len(data) if data else None}')
 
-        self._log.debug(f'__PREPARE headers={updated_headers} <{updated_url}>')
-        request = requests.Request(method=method,
-                                   url=updated_url,
-                                   headers=updated_headers,
-                                   data=data,
-                                   auth=self._session.auth)
-        prepared_request = self._session.prepare_request(request)
+        self._log.debug(f'__PREPARE REQUEST')
+        if not params:
+            params = dict()
+        if isinstance(data, list) or isinstance(data, dict):
+            data = json.dumps(data)
+        request = self._prepare_request(method=method, url=url, data=data, params=params)
 
-        self._log.debug(f'__SEND headers={request.headers} <{request.url}>')
-        response = self._session.send(request=prepared_request,
-                                      cert=self._cert,
-                                      verify=self._verify,
-                                      allow_redirects=False)
-        redirects = 0
-        while response.is_redirect:
-            redirects += 1
-            self._log.debug(f'__REDIRECT {redirects} <{response.next.url}>')
-            if redirects > self._session.max_redirects:
-                raise requests.HTTPError('Too many re-directions')
-
-            self._log.debug(f'__PREPARE headers={response.headers} <{response.next.url}>')
-            self._session.rebuild_auth(prepared_request=response.next, response=response)
-            response.next.prepare_headers(updated_headers)
-            response.next.prepare_cookies(response.cookies)
-            response.next.url = self._update_params(url=response.next.url, params=updated_params)
-
-            self._log.debug(f'__SEND: headers={updated_headers} <{response.next.url}>')
-            response = self._session.send(request=response.next,
-                                          verify=self._verify,
-                                          allow_redirects=False)
+        self._log.debug(f'__SEND REQUEST headers={request.headers} <{request.url}>')
+        session = requests.Session()
+        prepared_request = session.prepare_request(request)
+        response = session.send(request=prepared_request,
+                                cert=self._auth.cert,
+                                verify=self._auth.verify,
+                                allow_redirects=False)
 
         self._log.debug(f'__RECEIVED {response.text}')
         return response
